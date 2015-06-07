06af492373602657ee19916acb649210965a7da0
Christopher Friedt <chrisfriedt@gmail.com>
2015-06-07 08:53:30 -0400

    cleaned up discoball.h, linux_types.h

--- include/public/discoball/discoball.h
+++ include/public/discoball/discoball.h
@@ -27,6 +27,10 @@
 #ifndef DISCOBALL_DISCOBALL_H_
 #define DISCOBALL_DISCOBALL_H_
 
+/****************************************************************************
+ *                       STANDARD / SYSTEM HEADERS
+ ****************************************************************************/
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -40,6 +44,42 @@
 #include <errno.h>
 
 #include <string.h>
+
+/****************************************************************************
+ *                       LINUX INCLUDES
+ ****************************************************************************/
+
+#ifdef __APPLE__
+
+#ifndef _LINUX_ERR_H
+#define _LINUX_ERR_H
+#endif /* _LINUX_ERR_H */
+#ifndef _UAPI_LINUX_TYPES_H
+#define _UAPI_LINUX_TYPES_H
+#endif /* _UAPI_LINUX_TYPES_H */
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+#endif /* _LINUX_TYPES_H */
+#ifndef _LINUX_STDDEF_H
+#define _LINUX_STDDEF_H
+#endif /* _LINUX_STDDEF_H */
+#ifndef _LINUX_SEQ_FILE_H
+#define _LINUX_SEQ_FILE_H
+#endif /* _LINUX_SEQ_FILE_H */
+#ifndef _LINUX_RADIX_TREE_H
+#define _LINUX_RADIX_TREE_H
+#endif /* _LINUX_RADIX_TREE_H */
+#ifndef _LINUX_RADIX_TREE_H
+#define _SECURE__STRING_H_
+#endif /* _LINUX_RADIX_TREE_H */
+
+#include <discoball/linux_types.h>
+
+#endif
+
+////////////
+// PINCTRL
+////////////
 
 #define CONFIG_GENERIC_PINCONF
 #define CONFIG_OF
@@ -55,5 +95,11 @@
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/pinctrl/pinctrl-state.h>
 #include <linux/pinctrl/pinmux.h>
+
+/****************************************************************************
+ *                       DISCOBALL INCLUDES
+ ****************************************************************************/
+
+#include <discoball/packet.h>
 
 #endif /* DISCOBALL_DISCOBALL_H_ */
